30e483e51ec1414c6e9b81cc04a52f6a44ceec0f
Jakub <kisiel.jakub00@gmail.com>
2022-02-15 23:47:26 +0100

    Added build automation

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -jar /apps/Gateaway-0.0.1-SNAPSHOT.jar &
-java -jar /apps/Jobs-0.0.1-SNAPSHOT.jar &
-java -jar /apps/Workers-0.0.1-SNAPSHOT.jar
+java -jar /apps/Gateaway.jar &
+java -jar /apps/Jobs.jar &
+java -jar /apps/Workers.jar
